ced56fd71497efad74bcb6639947f064609ddbc9
samLynux <samuelbisnes@gmail.com>
2022-04-11 13:37:47 +0700

    14

--- src/Home/OutfitIdeas/Category.tsx
+++ src/Home/OutfitIdeas/Category.tsx
@@ -1,6 +1,6 @@
 import  React, { useState } from 'react';
-import { View } from 'react-native';
-import { BorderlessButton } from 'react-native-gesture-handler';
+import { StyleSheet, View } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import { Box, Text } from '../../components';
 
 interface CategoryProps {
@@ -17,22 +17,39 @@ const Category = ({category: {color: backgroundColor,title}}: CategoryProps) => 
     const [selected, setSelected] = useState(false)
     return (
         <>
-        <BorderlessButton onPress={() => setSelected((prev) => !prev)}>
+        <TouchableWithoutFeedback onPress={() => setSelected((prev) => !prev)}>
         <Box 
             marginLeft="m" alignItems="center"
             marginTop="m"
         >
+            <Box
+                width={75} height={120}
+                justifyContent="center"
+                alignItems="center"
+            >
             <View style={{
-                width: 60, height: 60, borderRadius: 30,
+                width: 60, height: 60,
+                borderRadius: 30,
+                
                 backgroundColor
             }}>
+                
+            {selected && (
+                <View style={{
+                    ...StyleSheet.absoluteFillObject,
+                    borderWidth:1,
+                    borderRadius: 60,
+                    borderColor: "black"
+                }}/>
+            )}
 
             </View>
             <Text textAlign="center" marginTop="s">
                 {title}
             </Text>
+            </Box>
         </Box>
-        </BorderlessButton>
+        </TouchableWithoutFeedback>
         </>
     )
 }
